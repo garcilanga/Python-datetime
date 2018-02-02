5ebeafef9b1867e0f90d40ee8c50394e83c07bc7
Juan García <33761598+garcilanga@users.noreply.github.com>
2018-02-02 11:26:10 +0100

    Update strftime.py

--- strftime.py
+++ strftime.py
@@ -1,3 +1,3 @@
 import datetime
-str_fecha = datetime.datetime.strftime(datetime.datetime(2017, 5, 3), '%d %b %Y')
+str_fecha = datetime.datetime.strftime(datetime.datetime(2018, 5, 3), '%d %b %Y')
 print str_fecha
